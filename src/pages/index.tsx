499a2dc741b28e27b55f623f7d89dd7276bc437c
Arpit Gupta <gupta98960@gmail.com>
2023-11-08 12:12:52 +0530

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
 import KanbanBoard from "@/components/KanbanBoard";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
